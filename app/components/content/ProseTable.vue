76a9e2caec4a014eeb74314bdecfea99f324f55d
Wing <114337707+wing-0902@users.noreply.github.com>
2026-06-23 13:26:44 +0000

    fmt [ci skip]

--- app/components/content/ProseTable.vue
+++ app/components/content/ProseTable.vue
@@ -1,24 +1,25 @@
 <template>
   <div w-full overflow-x-auto>
-    <table class='tableRoot'>
+    <table class="tableRoot">
       <slot />
     </table>
   </div>
 </template>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .tableRoot {
   width: 100%;
   table-layout: auto;
   border-collapse: separate;
-  
+
   border-spacing: 1px 0.5px; /* 間 */
 
   border-top: 2px solid white;
   border-bottom: 2px solid white;
 
   /* セル全部 */
-  :deep(th), :deep(td) {
+  :deep(th),
+  :deep(td) {
     padding: 0.75rem 1rem;
     white-space: nowrap;
   }
@@ -26,7 +27,7 @@
   /* 見出し */
   :deep(th) {
     border-bottom: 0.6px solid white;
-    border-left: 1px solid rgba(255,255,255,0.5);
+    border-left: 1px solid rgba(255, 255, 255, 0.5);
     &:first-child {
       border-left-width: 0;
     }
@@ -36,12 +37,12 @@
   :deep(tbody tr) {
     td {
       border-top: 1px solid rgba(255, 255, 255, 0.5);
-      border-left: 1px solid rgba(255,255,255,0.5);
+      border-left: 1px solid rgba(255, 255, 255, 0.5);
       &:first-child {
         border-left-width: 0;
       }
     }
-    
+
     /* 最初の１行 */
     &:first-child td {
       border-top-color: white;
